9cfa23a21542d8e2009cec8acd331785a554bf16
Birendra <birendra@rudrainnovative.in>
2023-07-24 19:01:24 +0530

    navbar,loginWithGoogle

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { } from '@angular/fire/auth'
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+   
+
+  login() {
+  
+}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCOgSg-UXF2rk3FmSZENNz9L52ZOskS3PI',
+    authDomain: 'shopping-cart-f0f06.firebaseapp.com',
+    projectId: 'shopping-cart-f0f06',
+    storageBucket: 'shopping-cart-f0f06.appspot.com',
+    messagingSenderId: '669212613613',
+    appId: '1:669212613613:web:a044fdff53ec4b0ee3e5e4',
+  },
+};
